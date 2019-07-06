18565b72da2b4c7dbc687ca803e1ce6d79bb5aec
Dinesh Kumar <aminfow01@gmail.com>
2019-07-06 17:07:10 +0530

    remove static folder from connection app

--- connection/static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  // $('a#calculate').bind('click', function() {
-    $.getJSON($SCRIPT_ROOT + '/ajax_request', function(data) {
-      console.log("Ajax Called");
-      $("#result").text(data.result);
-    });
-    return false;
-
-  });
